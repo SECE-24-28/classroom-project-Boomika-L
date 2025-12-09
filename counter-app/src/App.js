e0b82537775376d619dc8932986cb0dbc7af13c0
Boomika-L <boomika.l2024cse@sece.ac.in>
2025-12-09 11:31:44 +0530

    functions in react

--- counter-app/src/App.js
+++ counter-app/src/App.js
@@ -1,12 +1,21 @@
 // import Component from "./Component";
 // import {Component1,Component2,Component3,Component4,Component5} from "./Component"
-import Grandfather from './Grandfather';
+// import Grandfather from './Grandfather';
 import './App.css'
 
 function App() {
 
-  let msg ="hii good morning"
-  // let val = 21;
+  // let msg ="hii good morning"
+  let val = 0;
+  const incre = () =>{
+    val = val+1;
+    console.log("Increment button clicked",val);
+  };
+  const decre = () =>{
+    val = val-1;
+    console.log("decrement button clicked",val);
+  };
+
   return (
     <div className="App">
     <h1>Learning a react</h1>
@@ -16,7 +25,15 @@ function App() {
     <Component3></Component3>
     <Component4></Component4>
     <Component5></Component5> */}
-    <Grandfather a={msg}></Grandfather>
+    <div className = "counter">
+       <h1>{val}</h1>
+       <div className='buttons'>
+        <button onClick ={incre} className='inc'>Increment</button>
+        <button onClick = {decre} className='dec'>Decrement</button>
+       </div>
+
+    </div>
+    {/* <Grandfather a={msg}></Grandfather> */}
 
     
     
